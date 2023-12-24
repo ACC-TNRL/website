9f2fe1146e084e961a129563e82f1ddb16d340d7
rwvansoest <154540356+ACC-TNRL@users.noreply.github.com>
2023-12-24 13:22:57 +0100

    Update

--- scripts.js
+++ scripts.js
@@ -7,3 +7,23 @@ function toggleMobileMenu() {
     mobileMenu.style.display = isMenuVisible ? 'none' : 'block';
     mobileIcon.classList.toggle('active', !isMenuVisible);
 }
+
+$(document).ready(function() {
+    // Stijlen toepassen op de actieve tab bij het laden van de pagina
+    $('.nav-pills .nav-link.active').css({
+        'background-color': '#FFA500',
+        'color': '#000000'
+    });
+
+    // Stijlen toepassen wanneer een nieuwe tab wordt geselecteerd
+    $('.nav-pills .nav-link').on('shown.bs.tab', function() {
+        $('.nav-pills .nav-link').css({
+            'background-color': '',
+            'color': ''
+        });
+        $(this).css({
+            'background-color': '#FFA500',
+            'color': '#000000'
+        });
+    });
+});
